ec51dda1dc32d573f5b514ba9d83a2f279182561
ngeTaco <thealdo.531@gmail.com>
2024-05-27 22:19:30 -0600

    initial commit

---
+++ server.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import session from 'express-session';
+import morgan from 'morgan';
+import nunjucks from 'nunjucks';
+
+const app = express();
+const port = '4090';
+
+// Middleware
+app.use(morgan('dev'));
+// post reqs
+app.use(express.urlencoded({extended: false}))
+app.use(express.static('public'))
+app.use(session({
+    secret: 'itsasecret',
+    saveUninitialized: true,
+    resave: false
+}))
+nunjucks.configure('views', {
+    autoescape: true,
+    express: app
+})
+app.get('/', (req, res) => {
+    res.render('index.html')
+})
+
+app.listen(port, () => {
+    console.log(`App listening on port http://localhost:${port}`);
+})
