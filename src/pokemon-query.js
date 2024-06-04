095addaf4198a7df43d46917919341c60f681f7f
ngeTaco <thealdo.531@gmail.com>
2024-06-03 22:30:11 -0600

    store info on pokemon in object and cleaned up difficulty data

--- src/pokemon-query.js
+++ src/pokemon-query.js
@@ -1,16 +1,24 @@
 import axios from 'axios';
 
+//NOTE: create url with a random number for pokeapi request
 let randomPkmnNum = Math.floor(Math.random() * 1025) + 1;
 let pkmnUrl = `https://pokeapi.co/api/v2/pokemon/${randomPkmnNum}`;
 
-console.log(pkmnUrl);
+//console.log(pkmnUrl);
 
+//NOTE: pokemon information to be stored in an object
+let pkmnInfo = {};
+//NOTE: GET pokemon data and destructure into Name, default Sprite, and Attack Stat
 axios.get(pkmnUrl).then((response) => {
-    let pkmnName = response.data.name;
-    let pkmnSprite = response.data.sprites.front_default;
-    let pkmnAttack = response.data.stats[1].base_stat;
+    pkmnInfo = response.data;
+    const { name, sprites: { front_default }, stats: [, { base_stat }] } = pkmnInfo;
+    console.log({ name, front_default, base_stat })
 
-    console.log(pkmnName);
-    console.log(pkmnSprite);
-    console.log(pkmnAttack);
+    // let pkmnName = response.data.name;
+    // let pkmnSprite = response.data.sprites.front_default;
+    // let pkmnAttack = response.data.stats[1].base_stat;
+
+    // console.log(pkmnName);
+    // console.log(pkmnSprite);
+    // console.log(pkmnAttack);
 })
