17e0cd990f2e0e494d7693d6418cda99498a1852
ngeTaco <thealdo.531@gmail.com>
2024-06-03 22:14:56 -0600

    got axios.get for pokemon information working

--- src/pokemon-query.js
+++ src/pokemon-query.js
@@ -4,3 +4,13 @@ let randomPkmnNum = Math.floor(Math.random() * 1025) + 1;
 let pkmnUrl = `https://pokeapi.co/api/v2/pokemon/${randomPkmnNum}`;
 
 console.log(pkmnUrl);
+
+axios.get(pkmnUrl).then((response) => {
+    let pkmnName = response.data.name;
+    let pkmnSprite = response.data.sprites.front_default;
+    let pkmnAttack = response.data.stats[1].base_stat;
+
+    console.log(pkmnName);
+    console.log(pkmnSprite);
+    console.log(pkmnAttack);
+})
